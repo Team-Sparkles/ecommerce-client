f9c67183c3af37d7fcc98887aeee258a7327ea47
terichadbourne <terichad@gmail.com>
2018-07-18 23:12:59 -0400

    Remove item from order
    - In `orders-events.js`, add event handler that listens for click on a
    `remove` button in the shopping cart items list
    - When clicked, run the new `removeItemFromOrder` function to remove
    item from order, show the order again, and rewrite list of items to page
    - Note that the item removed will be the first one in the list that
    matches the item you want to remove (eg first of two sharks in cart),
    not necessarily the same line you click on. Doing that would require
    mapping a data-index field to the array before passing it to Handlebars

--- assets/scripts/orders/orders-events.js
+++ assets/scripts/orders/orders-events.js
@@ -8,6 +8,8 @@ const store = require('../store')
 // event handlers for...
 const addHandlers = function () {
   $('#marketplace').on('click', '.tile-cart-button', addItemToOrder)
+  $('#cart-items').on('click', '.remove-item-button', removeItemFromOrder)
+  $('#cart-items-test').on('click', '.remove-item-button', removeItemFromOrder)
 }
 
 const onCreateOrder = function () {
@@ -22,6 +24,8 @@ const onCreateOrder = function () {
     .then(ordersUi.createOrderSuccess)
     .catch(ordersUi.createOrderError)
 }
+
+
 
 const addItemToOrder = function () {
   // find item ID from button clicked
@@ -70,8 +74,62 @@ const addItemToOrder = function () {
     .catch(ordersUi.showOrderError)
 }
 
+
+// removes a specific item from order when button is clicked
+const removeItemFromOrder = function () {
+  console.log('you want to remove an item from your order')
+  // find item ID from button clicked
+  const itemId = $(this).attr('data-id')
+  console.log('itemId is: ', itemId)
+  // find order id based on the contents of the first element with class
+  // .order-id
+  const orderId = $('.order-id').html()
+  console.log('order id is: ', orderId)
+  // get order in its current form from DB (which includes populated items)
+  ordersApi.showOrder(orderId)
+    .then((response) => {
+      // create an array for item id references
+      const itemsIdsOnlyArray = []
+      // loop through items, which come in populated, grab their ids, and push
+      // only the ids to the array
+      response.order.items.forEach((item) => {
+        itemsIdsOnlyArray.push(item._id)
+      })
+      console.log('updated items array w/o extra details : ', itemsIdsOnlyArray)
+      // find index of first element in array that matches the item ID you
+      // asked to remove
+      const firstMatchIndex = itemsIdsOnlyArray.findIndex(item => item === itemId)
+      console.log('need to delete from array the first match at position ', firstMatchIndex)
+      // remove one element from array at that index
+      itemsIdsOnlyArray.splice(firstMatchIndex, 1)
+      console.log('updated item list with one removed is: ', itemsIdsOnlyArray)
+      // set up data to update order with, using the items array made up of
+      // only IDs, not populated objects
+      const data = {
+        order: {
+          items: itemsIdsOnlyArray,
+          checkoutComplete: false
+        }
+      }
+      console.log('updated response about to be saved is: ', data)
+      // call the function to update the order, passing in the order number and
+      // the revised data (minus removed item)
+      ordersApi.updateOrder(orderId, data)
+        // because our update route doesn't return any data, we have to
+        // do another show to retrieve the updated order with populated items
+        .then(() => ordersApi.showOrder(orderId))
+        // send the order from the response to the updateCartDetails function
+        // to be printed to page
+        .then(response => ordersUi.updateCartDetails(response.order))
+        // log any errors along the way
+        .catch(error => console.error)
+    })
+    .catch(ordersUi.showOrderError)
+}
+
 module.exports = {
   addHandlers: addHandlers,
   addItemToOrder: addItemToOrder,
-  onCreateOrder: onCreateOrder
+  onCreateOrder: onCreateOrder,
+  removeItemFromOrder: removeItemFromOrder
 }
