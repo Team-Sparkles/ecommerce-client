0f827cc691c409a8e98a7da67b306fc090bacbee
terichadbourne <terichad@gmail.com>
2018-07-19 17:45:50 -0400

    Moving all the files around to make Stripe work
    - Whatever Caleb told us to

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,4 @@
-'use strict'
+
 const events = require('./events')
 const authEvents = require('./auth/auth-events')
 const ordersEvents = require('./orders/orders-events')
@@ -14,4 +14,35 @@ $(() => {
   authEvents.addHandlers()
   events.getTiles()
   ordersEvents.addHandlers()
+  console.log('StripeCheckout is ', StripeCheckout)
+  // stripe stuff previously in index.html
+  const checkoutHandler = StripeCheckout.configure({
+    key: 'pk_test_ikhOV44uVLXegqqnKBGr490P',
+    locale: 'auto'
+  })
+  const button = document.getElementById('buttonCheckout')
+  button.addEventListener('click', function (ev) {
+    checkoutHandler.open({
+      name: 'Nozama Aquatic Aquisitions',
+      description: 'Your Purchase',
+      token: handleToken
+    })
+  })
+  function handleToken (token) {
+    fetch('http://localhost:4741/charge', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(token)
+    })
+      .then(output => {
+        console.log('output is ', output)
+        console.log('output.id is ', output.id)
+        console.log('output.body is ', output.body)
+        console.log('output.status is ', output.status)
+        if (output.status === 200) {
+          document.getElementById('shop').innerHTML = '<p>Purchase complete!</p>'
+          console.log('about to run markOrderComplete')
+        }
+      })
+  }
 })
